fc8cf865567cf816a4a9882b8b5a302db38582bc
Tim Corley <tcorley215@gmail.com>
2020-03-06 13:19:12 -0500

    Initial commit. Project setup & installs. Basic electron app launching & relaoding on changes.

---
+++ src/start.js
@@ -0,0 +1,47 @@
+const electron = require('electron');
+const app = electron.app;
+const isDev = require('electron-is-dev');
+require('electron-reload');
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  // LAUNCH APP @ TOP-RIGHT
+  let display = electron.screen.getPrimaryDisplay();
+  let width = display.bounds.width;
+  mainWindow = new BrowserWindow({
+    width: 600,
+    x: width - 600,
+    y: 0,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
