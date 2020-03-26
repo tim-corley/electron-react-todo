6fe27be8a3ecf6f2cf608121f4fa514142c990c6
Tim Corley <tcorley215@gmail.com>
2020-03-26 15:34:17 -0400

    refactor ToDoForm & App to use functional components w/ useState hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import MainCard from './components/MainCard';
+import React from 'react';
+import ToDoForm from './components/ToDoForm';
 import './styles/app.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="flex h-screen bg-local img-bg">
-        <MainCard />
+const App = () => {
+  return (
+    <div className="flex h-screen bg-local img-bg">
+      <div className="m-auto h-auto w-11/12 my-8 rounded shadow-lg bg-minwarm-lightsand opacity-75">
+        <ToDoForm saveTodo={console.warn} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -1,53 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { IoIosAddCircleOutline } from 'react-icons/io';
 import shortid from 'shortid';
 
-class ToDoForm extends Component {
-  state = {
-    text: ''
-  };
+const ToDoForm = ({ saveTodo }) => {
+  const [value, setValue] = useState('');
 
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    // this is the addTodos method
-    this.props.onSubmit({
-      id: shortid.generate(),
-      text: this.state.text,
-      complete: false
-    });
-    this.setState({
-      text: ''
-    });
-  };
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="flex items-center border-b border-b-2 border-white">
-          <div className="flex w-full px-4">
-            <input
-              className="appearance-none bg-transparent border-none w-full text-midnightink-headline text-xl placeholder-text-indigo-900 mr-3 leading-tight focus:outline-none"
-              name="text"
-              onChange={this.handleChange}
-              value={this.state.text}
-              placeholder="add a new todo..."
-            />
-            <button
-              className="w-1/4 px-2 py-4 mb-2 focus:outline-none"
-              onClick={this.handleSubmit}
-            >
-              <IoIosAddCircleOutline className="flex text-3xl m-auto text-midnightink-button" />
-            </button>
-          </div>
+  return (
+    <form
+      onSubmit={event => {
+        event.preventDefault();
+        saveTodo(value);
+      }}
+    >
+      <div className="flex items-center border-b border-b-2 border-white">
+        <div className="flex w-full px-4">
+          <input
+            className="appearance-none bg-transparent border-none w-full text-midnightink-headline text-xl placeholder-text-indigo-900 mr-3 leading-tight focus:outline-none"
+            name="text"
+            onChange={event => {
+              setValue(event.target.value);
+            }}
+            value={value}
+            placeholder="add a new todo..."
+          />
+          <button
+            className="w-1/4 px-2 py-4 mb-2 focus:outline-none"
+            onClick={event => {
+              console.log(event.target);
+            }}
+          >
+            <IoIosAddCircleOutline className="flex text-3xl m-auto text-midnightink-button" />
+          </button>
         </div>
-      </form>
-    );
-  }
-}
+      </div>
+    </form>
+  );
+};
 
 export default ToDoForm;
